b538c8fcfa02ed5e1d83edab6587c0930363676d
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-12-02 00:17:48 +0200

    feat: task 01 completed

--- src/js/01-js-drum-kit.js
+++ src/js/01-js-drum-kit.js
@@ -1,1 +1,25 @@
+const refs = {
+  body: document.querySelector('body'),
+  keys: document.querySelectorAll('.key'),
+};
 
+refs.body.addEventListener('keydown', onKeyDown);
+refs.keys.forEach(key =>
+  key.addEventListener('transitionend', removeTransition)
+);
+
+function onKeyDown(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  if (!audio) return;
+  audio.currentTime = 0; //rewinds to start of the audio
+  audio.play();
+
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+  if (!key) return;
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  this.classList.remove('playing');
+}
