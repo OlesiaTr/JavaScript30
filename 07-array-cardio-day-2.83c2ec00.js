63ec1c675096a178361157d9bd3525fb33db1dae
OlesiaTr <104642736+OlesiaTr@users.noreply.github.com>
2022-12-12 15:26:49 +0000

    Deploying to gh-pages from @ OlesiaTr/JavaScript30@2be4812a9aa4bb5baeb6c37b0126fe6170571215 🚀

---
+++ 07-array-cardio-day-2.83c2ec00.js
@@ -0,0 +1,2 @@
+const e=[{name:"Wes",year:1988},{name:"Kait",year:1986},{name:"Irv",year:1970},{name:"Lux",year:2015}],i=[{text:"Love this!",id:523423},{text:"Super good",id:823423},{text:"You are the best",id:2039842},{text:"Ramen is my fav food ever",id:123523},{text:"Nice Nice Nice!",id:542328}],t=(new Date).getFullYear(),a=([...e].some((e=>t-e.year>=19)),[...e].every((e=>t-e.year>=19)),[...i].find((e=>823423===e.id)),[...i].findIndex((e=>823423===e.id)));i.slice(0,a),i.slice(a+1);
+//# sourceMappingURL=07-array-cardio-day-2.83c2ec00.js.map
--- 07-array-cardio-day-2.c9760d1b.js
+++
@@ -1,2 +0,0 @@
-
-//# sourceMappingURL=07-array-cardio-day-2.c9760d1b.js.map
